94aa3733ed5c836dea53c981bf6e852d56855535
VyacheslavE <silver_2004@rambler.ru>
2017-07-11 23:51:57 +0300

    Add files via upload

---
+++ Lesson2_coaching3/menu.php
@@ -0,0 +1,11 @@
+﻿<?php
+	$menu = '
+		<ul>
+		    <li><a href="Index.php">После Марса</a></li>
+		    <li><a href="page1.php">Останки гигантов</a></li>
+		    <li><a href="page2.php">Жизнь на марсе</a></li>
+		    <li><a href="page3.php">Туннель под пирамидой</a></li>
+		    <li><a href="page4.php">Лунный дом</a></li>
+		</ul>
+		';
+?>
---
+++ Lesson2_coaching3/page2.php
@@ -0,0 +1,18 @@
+﻿<?php include('data2.php'); ?>
+<?php include('menu.php'); ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+         <link rel="shortcut icon" href="img/icon.png" type="image/png">
+	 <link rel="stylesheet" href="css/normalize.css">
+         <link rel="stylesheet" href="css/main.css">
+         <title><?php echo $title; ?></title>
+    </head>
+    <body>
+	<?php echo $menu; ?>
+        <h1><?php echo $title; ?></h1>
+	<?= $postText; ?>
+	<?= $datePost; ?>
+    </body>
+</html>
